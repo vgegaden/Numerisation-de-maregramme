08c562ed9fcc14379ee31cc288817645ab7140a8
vgegaden <vgegaden@etudiant.univ-lr.fr>
2026-04-20 10:23:50 +0200

    update test2.py en ajoutant kdtree + logique pour essayer de trouver la courbe jour 1

--- test2.py
+++ test2.py
@@ -4,12 +4,14 @@
 #ajout logique j+1
 #ajout régression linéaire
 #ajout logique pour trouver la courbe du jour 1
+#ajout kdtree 
 
 
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import medfilt
+from scipy.spatial import KDTree #pour améliorer la vitesse
 
 def extraction_reconstruction_test1(chemin_img):
     img = cv2.imread(chemin_img)
@@ -23,8 +25,11 @@ def extraction_reconstruction_test1(chemin_img):
 
     cv2.imwrite("debug_1_extraction_brute.png", masque_bleu)
 
-    kernel = np.ones((3, 3), np.uint8)
-    masque_propre = cv2.morphologyEx(masque_bleu, cv2.MORPH_CLOSE, kernel)
+    kernel_gros = np.ones((5, 5), np.uint8)
+    kernel_fin = np.ones((3, 3), np.uint8)
+
+    masque_propre= cv2.dilate(masque_bleu, kernel_gros, iterations = 1)
+    masque_propre = cv2.morphologyEx(masque_propre, cv2.MORPH_CLOSE, kernel_fin)
     masque_propre = cv2.medianBlur(masque_propre, 5)
 
     cv2.imwrite("debug_2_extraction_propre.png", masque_propre)
@@ -37,94 +42,105 @@ def extraction_reconstruction_test1(chemin_img):
     contours, _ = cv2.findContours(squelette, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     points_list = []
     for i in contours:
-        for point in i:
-            points_list.append(tuple(point[0]))
-    points_set = set(points_list)
+        if cv2.arcLength(i, True) > 50:
+            for point in i:
+                points_list.append(tuple(point[0]))
+    
     if not points_list:
         return [0],[0],[0]
     
+    points_set = set(points_list)
     x_reconstruit = []
     y_reconstruit = []
 
+    #creer un index spatial pour la recherche rapide
+    tree = KDTree(points_list)
+
+
     #point le plus a gauche
-    point_gauche = min(points_list, key =lambda p: p[0])
+    current_point = min(points_list, key =lambda p: p[0])
     largeur_image = img.shape[1]
 
     #logique recherche courbe jour 1
     seuil_iso = 15 #distance recherche voisin
     candidats_depart = []
     for p in points_list : 
-        if p[0] > 50:
-            voisins_gauche = [v for v in points_list if (p[0] - seuil_iso) < v[0] < p[0] and abs(v[1] - p[1]) < 10]
-            voisins_droite = [v for v in points_list if p[0] < v[0] < (p[0] + seuil_iso) and abs(v[1] - p[1]) < 10]
-            if not voisins_gauche and len(voisins_droite) > 5 :
+        if p[0] > 30: #30px pour ne pas commencer au bord
+            #chercher si il y a des voisins dans un rayon de 30 px à gauche
+            idx_gauche = tree.query_ball_point(p, 30)
+            voisins_gauche = [points_list[i] for i in idx_gauche if points_list[i][0] < p[0] - 2]
+            #verifier qu'il y a bien une suite à droite (pour s'assurer que c'est une courbe)
+            idx_droite = tree.query_ball_point(p, 30)
+            voisins_droite = [points_list[i] for i in idx_droite if points_list[i][0] > p[0] + 2]
+            if len(voisins_gauche) == 0 and len(voisins_droite) > 10:
                 candidats_depart.append(p)
 
     if candidats_depart:
-        point_gauche = min(candidats_depart, key =lambda p: p[0])
-        print(f"Début automatique détecté à : {point_gauche}")
+        current_point = min(candidats_depart, key =lambda p: p[0])
+        print(f"Début automatique détecté à : {current_point}")
 
     else :
-        point_gauche = min(points_list, key =lambda p: p[0])
+        current_point = min(points_list, key =lambda p: p[0])
         print("Début par defaut (bord gauche)")
 
-    centre_image = largeur_image // 2 
+    #centre_image = largeur_image // 2 
     #pixels du bord
-    seuil_bord = 15
+    #seuil_bord = 15
     #nb de points pour la pente
-    nb_points_reg = 15
+    #nb_points_reg = 15
     #cpt de sauts j+1
     jour_actuel = 0
+    x_final, y_final = [], []
 
-    while points_set:
-        x_curr, y_curr = point_gauche
-        x_reconstruit.append(x_curr + (jour_actuel * largeur_image))
-        y_reconstruit.append(y_curr)
-        if point_gauche in points_set:
-            points_set.remove(point_gauche)
+    while True:
+        x_curr, y_curr = current_point
+        x_final.append(x_curr + (jour_actuel * largeur_image))
+        y_final.append(y_curr)
+        if current_point in points_set:
+            points_set.remove(current_point)
 
         #logique régression
         pente = 0
-        if len(x_reconstruit) > nb_points_reg:
-            pts_x = np.array(x_reconstruit[-nb_points_reg:])
-            pts_y = np.array(y_reconstruit[-nb_points_reg:])
-            A = np.vstack([pts_x, np.ones(len(pts_x))]).T
-            solution, _, _, _ = np.linalg.lstsq(A, pts_y, rcond=None)
-            pente = solution[0]
+        #calculer la pente sur les 20 derniers points
+        if len(x_final) > 20:
+            p = np.polyfit(x_final[-20:], y_final[-20:], 1)
+            pente = p[0]
+        
+        #predire prochain point avec inertie
+        cible_x = x_curr + 3
+        cible_y = y_curr + (pente*3)
 
         #logique j+1
         next_pt = None
         seuil_isolement = 10
         #si proche du bord droit
-        if x_curr > largeur_image - seuil_bord:
-            candidats_gauche = [p for p in points_set if p[0] < seuil_bord and abs(p[1] - y_curr) < 30]
-            if candidats_gauche:
-                next_pt = min(candidats_gauche, key=lambda p: abs(p[1] - y_curr))
+        if x_curr > largeur_image - 20:
+            #on cherche au bord gauche (x=5) à la même hauteur
+            dist, idx = tree.query([5, y_curr], k=10) #regarder les 10 plus proches
+            candidats = [points_list[i] for i in idx if points_list[i] in points_set]
+            if candidats:
+                next_pt = min(candidats, key=lambda p: abs(p[1] - y_curr))
                 jour_actuel += 1
-                print(f"saut détecté à y= {y_curr}")
+                print(f"passage au jour {jour_actuel+1}")
             
         if next_pt is None :
-            zone_a_scanner = [p for p in points_set if 0 < (p[0] - x_curr) < 6 and abs(p[1] - (y_curr + pente)) < 15]
-            if zone_a_scanner:
-                next_pt = min(zone_a_scanner, key=lambda p: abs(p[1] - (y_curr + pente)))
+            #chercher dans un petit rayon autour de la prédiction
+            dist, idx = tree.query([x_curr + 2, y_curr + pente], k=15)
+            candidats_suivi = [points_list[i] for i in idx if points_list[i] in points_set and points_list[i][0] > x_curr]
+            if candidats_suivi:
+                next_pt = min(candidats_suivi, key=lambda p: abs(p[1] - (y_curr + pente)))
 
         if next_pt:
-            point_gauche = next_pt
+            current_point = next_pt
         else:
-            if points_set:
-                point_gauche = min(points_set, key=lambda p: (p[0] - x_curr)**2 + (p[1] - y_curr)**2)
-                #si écart entre les 2 points trop grand, on stoppe
-                if (point_gauche[0] - x_curr)**2 > 900: break
-            else :
-                break
+            break
 
-    x=np.array(x_reconstruit)
-    y=np.array(y_reconstruit)
+    #x=np.array(x_reconstruit)
+    #y=np.array(y_reconstruit)
 
-    #filtrage sequentiel
-    y_smooth = medfilt(y, kernel_size=11)
+    #y_smooth = medfilt(y, kernel_size=11)
     
-    return x, y, y_smooth
+    return np.array(x_final), np.array(y_final), medfilt(y_final, kernel_size=15)
 
 chemin = "image/HPSC0869.tif"
 
@@ -132,12 +148,9 @@ try:
     x_val, y_raw, y_final = extraction_reconstruction_test1(chemin)
 
     plt.figure(figsize=(12, 6))
-    plt.scatter(x_val, y_raw, s=1, color='gray', alpha=0.5, label='Points bruts (Pixels)')
+    #plt.scatter(x_val, y_raw, s=1, color='gray', alpha=0.5, label='Points bruts (Pixels)')
     plt.plot(x_val, y_final, color='blue', label='Signal lissé (Combo 2)')
-    plt.title("Numérisation via Combo 2 (OpenCV + Filtrage)")
-    plt.xlabel("Temps (Pixels X)")
-    plt.ylabel("Hauteur (Pixels Y)")
-    plt.legend()
+    plt.title("Numérisation via Combo 2 (OpenCV + Filtrage + KDTree + suivi de pente)")
     plt.gca().invert_yaxis() 
     plt.savefig("resultat_numerisation.png")
     print("resultat enregistré")
